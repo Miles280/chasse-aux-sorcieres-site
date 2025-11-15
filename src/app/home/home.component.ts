1329ebd95e8c43cec65b73d7872be017abebbd98
Miles280 <loughsala.imed@gmail.com>
2025-11-15 23:14:21 +0100

    [DONE] Correction sur le carroussel effectué

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,15 +11,18 @@ interface Member {
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements AfterViewInit, OnInit {
-
   membres: Member[] = [
-    { nom: 'Miles', role: 'Animateur', bg: 'bg-purple-800' },
-    { nom: 'Zou :)', role: 'Animateur', bg: 'bg-indigo-800' },
-    { nom: 'Protecteur', role: 'Défenseur', bg: 'bg-green-800' },
-    { nom: 'Espion', role: 'Observateur', bg: 'bg-pink-800' },
+    { nom: 'Miles 🔮 ', role: 'Administrateur', bg: 'bg-purple-800' },
+    { nom: 'Agent du Chaos ', role: 'Développeuse', bg: 'bg-indigo-800' },
+    { nom: 'MrGrab ', role: 'Animateur', bg: 'bg-pink-800' },
+    { nom: 'Zou :) ', role: 'Animateur', bg: 'bg-pink-800' },
+    { nom: 'MOUSTIK', role: 'Animateur', bg: 'bg-pink-800' },
+    { nom: 'Xeoooo_ ', role: 'Analyste', bg: 'bg-green-800' },
+    { nom: 'LouDorée ', role: 'Graphiste', bg: 'bg-orange-800' },
+    { nom: 'Seii ', role: 'Rédacteur', bg: 'bg-gray-800' },
   ];
 
   memberDuplicated: Member[] = [];
@@ -29,23 +32,24 @@ export class HomeComponent implements AfterViewInit, OnInit {
   }
 
   ngAfterViewInit(): void {
-    const revealElements = document.querySelectorAll<HTMLElement>(".reveal-on-scroll");
+    const revealElements =
+      document.querySelectorAll<HTMLElement>('.reveal-on-scroll');
 
     const revealOnScroll = () => {
       const windowWidth = window.innerWidth;
-      
-      revealElements.forEach(el => {
+
+      revealElements.forEach((el) => {
         const elementLeft = el.getBoundingClientRect().left;
-        
+
         if (elementLeft < windowWidth - 100) {
-          el.classList.remove("opacity-0", "-translate-x-20");
-          el.classList.add("opacity-100", "translate-x-0");
+          el.classList.remove('opacity-0', '-translate-x-20');
+          el.classList.add('opacity-100', 'translate-x-0');
         }
       });
     };
 
-    window.addEventListener("scroll", revealOnScroll);
-    window.addEventListener("resize", revealOnScroll);
-    revealOnScroll(); 
+    window.addEventListener('scroll', revealOnScroll);
+    window.addEventListener('resize', revealOnScroll);
+    revealOnScroll();
   }
 }
