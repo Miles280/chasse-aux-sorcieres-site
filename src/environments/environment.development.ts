ca98492b6c253d900ca19455d892b397a9f91b78
Miles280 <loughsala.imed@gmail.com>
2025-11-23 00:01:12 +0100

    [DONE] Gitignore le fichier d'environnement de development

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000/api',
-  discordClientId: '1440736031786799104',
-  discordRedirectUri: 'http://localhost:4200/auth/discord/callback',
-};
