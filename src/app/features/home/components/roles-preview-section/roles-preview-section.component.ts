77be6e18d58ea55143f4c414d76642abd54aea5d
Miles280 <loughsala.imed@gmail.com>
2026-02-02 20:11:24 +0100

    [STYLE] Modification de toute la page d'accueil, suppression des effets de séparation de sections et ajout de quelques couleurs représentatif du jeu.

--- src/app/features/home/components/roles-preview-section/roles-preview-section.component.ts
+++ src/app/features/home/components/roles-preview-section/roles-preview-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-roles-preview-section',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './roles-preview-section.component.html',
   styleUrl: './roles-preview-section.component.css',
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: { fontFamily: {
+    extend: { 
+      fontFamily: {
         title: ['Sekuya', 'serif'],
         detailUnderTitle: ['Barlow Condensed', 'sans-serif'],
         test: ['Cinzel Decorative', 'sans-serif'],
         test2: ['Cinzel', 'serif']
-    }},
+      },
+    }
   },
   plugins: [],
 }
