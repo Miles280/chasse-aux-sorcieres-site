0cac72c39d1fb8e4f42c646ad7a82c400d81fcb5
Miles280 <loughsala.imed@gmail.com>
2026-02-04 02:06:15 +0100

    [FIX] Correction du carrousel et changement du background de la page d'accueil

--- src/app/features/home/components/staff-section/staff-section.component.ts
+++ src/app/features/home/components/staff-section/staff-section.component.ts
@@ -1,22 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-staff-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './staff-section.component.html',
   styleUrl: './staff-section.component.css',
 })
 export class StaffSectionComponent {
   membres = [
-    { nom: 'Miles 🔮', role: 'Administrateur', bg: 'bg-purple-800' },
-    { nom: 'Agent du Chaos', role: 'Développeuse', bg: 'bg-indigo-800' },
-    { nom: 'MrGrab', role: 'Animateur', bg: 'bg-pink-800' },
-    { nom: 'Zou :)', role: 'Animateur', bg: 'bg-pink-800' },
-    { nom: 'MOUSTIK', role: 'Animateur', bg: 'bg-pink-800' },
-    { nom: 'Xeoooo_', role: 'Analyste', bg: 'bg-green-800' },
-    { nom: 'LouDorée', role: 'Graphiste', bg: 'bg-orange-800' },
+    {
+      nom: 'Miles 🔮',
+      role: 'Administrateur',
+      bg: 'bg-purple-800',
+      image: 'staff/miles.gif',
+    },
+    {
+      nom: 'Agent du Chaos',
+      role: 'Développeuse',
+      bg: 'bg-indigo-800',
+      image: 'staff/gridlock.png',
+    },
+    { nom: 'MrGrab', role: 'Animateur', bg: 'bg-pink-800', image: '' },
+    { nom: 'Zou :)', role: 'Animateur', bg: 'bg-pink-800', image: '' },
+    { nom: 'MOUSTIK', role: 'Animateur', bg: 'bg-pink-800', image: '' },
+    { nom: 'Xeoooo_', role: 'Analyste', bg: 'bg-green-800', image: '' },
+    {
+      nom: 'LouDorée',
+      role: 'Graphiste',
+      bg: 'bg-orange-800',
+      image: 'staff/loudoree.png',
+    },
   ];
-
-  ngOnInit(): void {}
 }
