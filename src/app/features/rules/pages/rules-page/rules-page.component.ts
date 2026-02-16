a2af4a46fc20eb20eec4a30bbda3543b55986552
Gridlock242 <Neltharion@live.fr>
2026-02-16 19:44:45 +0100

    [FIX] Changement du placement du dossier WIP, peut maintenant être appelé dans plusieurs pages et se trouve dans shared

--- src/app/features/rules/pages/rules-page/rules-page.component.ts
+++ src/app/features/rules/pages/rules-page/rules-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { WipComponent } from 'src/app/shared/components/wip/wip.component';
 
 @Component({
   selector: 'app-rules-page',
   standalone: true,
-  imports: [],
+  imports: [WipComponent],
   templateUrl: './rules-page.component.html',
   styleUrl: './rules-page.component.css'
 })
