56112d8f39210e4d9ba3eb25e14f26e1f4cba46c
Miles280 <loughsala.imed@gmail.com>
2026-02-18 17:45:36 +0100

    [FIX] Ajout de OnInit sur le composant de la page rôle

--- src/app/features/roles/pages/roles-page/roles-page.component.ts
+++ src/app/features/roles/pages/roles-page/roles-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Role } from 'src/app/core/models/role.model';
 import { RolesService } from 'src/app/core/services/roles.service';
 
@@ -9,7 +9,7 @@ import { RolesService } from 'src/app/core/services/roles.service';
   templateUrl: './roles-page.component.html',
   styleUrl: './roles-page.component.css',
 })
-export class RolesPageComponent {
+export class RolesPageComponent implements OnInit {
   private rolesService = inject(RolesService);
 
   activeTab: 'villageois' | 'sorcieres' | 'independants' = 'villageois';
