1e815f97b340151a73924b5a3c4474bb83472bf1
Gridlock242 <Neltharion@live.fr>
2026-03-29 21:37:06 +0200

    [FIX] Loader fonctionne

--- src/app/core/layout/navbar/navbar.component.ts
+++ src/app/core/layout/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import { Component, HostListener, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { environment } from '@env/environment';
 import { DiscordAuthService } from 'src/app/core/services/discord-auth.service';
+import { LoaderService } from 'src/app/core/services/loader.service'; // 1. Import du service
 
 @Component({
   selector: 'app-navbar',
@@ -14,6 +15,7 @@ import { DiscordAuthService } from 'src/app/core/services/discord-auth.service';
 export class NavbarComponent {
   private env = environment;
   public authService = inject(DiscordAuthService);
+  private loaderService = inject(LoaderService); // 2. Injection du service
 
   isScrolled = false;
   isMenuOpen = false;
@@ -27,12 +29,14 @@ export class NavbarComponent {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
-  // Fermer le menu quand on clique sur un lien
   closeMenu() {
     this.isMenuOpen = false;
   }
 
   loginWithDiscord() {
+    // 3. On affiche le loader immédiatement
+    this.loaderService.show();
+
     const url =
       `https://discord.com/oauth2/authorize` +
       `?client_id=${this.env.discordClientId}` +
@@ -40,10 +44,17 @@ export class NavbarComponent {
       `&redirect_uri=${encodeURIComponent(this.env.discordRedirectUri)}` +
       `&scope=guilds.members.read+email+identify`;
 
+    // 4. Redirection vers Discord
     window.location.href = url;
+
+    // Optionnel : Sécurité au cas où la redirection est bloquée par le navigateur
+    setTimeout(() => {
+      this.loaderService.hide();
+    }, 8000);
   }
 
   logout() {
     this.authService.logout();
+    this.closeMenu();
   }
 }
