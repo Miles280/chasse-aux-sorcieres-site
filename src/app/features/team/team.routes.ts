e08f74ef648d5022adaa6e6c758b87946cdca2cc
Miles280 <loughsala.imed@gmail.com>
2026-09-09 19:17:37 +0200

    [CHORE] Supression de l'ancienne page home ainsi que de la page Equipe non utilisé

--- src/app/features/team/team.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { TeamPageComponent } from './pages/team-page/team-page.component';
-
-export const TEAM_ROUTES: Routes = [
-  {
-    path: '',
-    component: TeamPageComponent,
-  },
-];
